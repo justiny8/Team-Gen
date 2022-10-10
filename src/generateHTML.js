3ae23a42fe8fc4cffcbd0b711eac5d11b9c27ff3
justiny8 <104675479+justiny8@users.noreply.github.com>
2022-10-09 20:43:36 -0700

    Add files via upload

---
+++ src/generateHTML.js
@@ -0,0 +1,145 @@
+//Intern card
+const generateIntern = function (intern) {
+    return `
+    <div class="col-4 mt-4">
+    <div class="card h-100">
+      <div class="card-header">
+                  <h3>${intern.name}</h3>
+                  <h4>Intern</h4>
+                  <i class="fas fa-user-graduate"></i>
+              </div>
+              <div class="card-body">
+                  <p class="id">ID: ${intern.id}</p>
+                  <p class="email">Email:<a href="mailto:${intern.email}">${intern.email}</a></p>
+                  <p class="school">School: ${intern.school}</p>
+              </div>
+      </div>
+  </div>
+      `;
+  };
+  
+  //Manager card
+  const generateManager = function (manager) {
+    return `
+      <div class="col-4 mt-4">
+          <div class="card h-100">
+              <div class="card-header">
+                  <h3>${manager.name}</h3>
+                  <h4>Manager</h4>
+                  <i class="fas fa-id-badge"></i>
+              </div>
+              <div class="card-body">
+                  <p class="id">ID: ${manager.id}</p>
+                  <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
+                  <p class="office">Office Number: ${manager.officeNumber}</p>
+              </div>
+          </div>
+      </div>
+      `;
+  };
+  
+  //Engineer card
+  const generateEngineer = function (engineer) {
+    return `
+      <div class="col-4 mt-4">
+          <div class="card h-100">
+              <div class="card-header">
+                  <h3>${engineer.name}</h3>
+                  <h4>Engineer</h4>
+                  <i class="fas fa-glasses"></i>
+              </div>
+              <div class="card-body">
+                  <p class="id">ID: ${engineer.id}</p>
+                  <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+                  <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+              </div>
+          </div>
+      </div>
+      `;
+  };
+  
+  generateHTML = (data) => {
+    pageArray = [];
+  
+    for (let i = 0; i < data.length; i++) {
+      const employee = data[i];
+      const role = employee.getRole();
+  
+      if (role === "Manager") {
+        const managerCard = generateManager(employee);
+  
+        pageArray.push(managerCard);
+      }
+  
+      if (role === "Engineer") {
+        const engineerCard = generateEngineer(employee);
+  
+        pageArray.push(engineerCard);
+      }
+  
+      if (role === "Intern") {
+        const internCard = generateIntern(employee);
+  
+        pageArray.push(internCard);
+      }
+    }
+  
+    // joins strings
+    const employeeCards = pageArray.join("");
+  
+    const generateTeam = generateTeamPage(employeeCards);
+    return generateTeam;
+  };
+  
+  // generates html
+  const generateTeamPage = function (employeeCards) {
+    return `
+    <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>team-profile-generator</title>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+      />
+      <link
+        rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+        crossorigin="anonymous"
+      />
+      <link rel="stylesheet" href="./style.css" />
+    </head>
+      <body>
+        <header>
+          <nav class="navbar" id="navbar">
+          <h1 class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text"
+          >Team Profile</h1>
+    </nav>
+  </header>
+      <main>
+            <div class="container">
+                <div class="row justify-content-center" id="team-cards">
+                    ${employeeCards}
+                </div>
+            </div>
+        </main>
+        
+    </body>
+    <script
+      src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+      integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+      integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
+      crossorigin="anonymous"
+    ></script>
+    </html>
+  `;
+  };
+  
+  module.exports = generateHTML;
